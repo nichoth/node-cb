56f530440041caab4d488d13116735a0e829a955
nichoth <nichoth@gmail.com>
2016-03-14 17:12:54 -0700

    fix

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports = function wrap(method) {
      // all args except cb
      var args = Array.prototype.slice.call(arguments, 0, -1);
      var cb = arguments[arguments.length-1];
-     return method(...args, res => cb(null, res), err => cb(err));
+     return method(...args, res => cb(null, res), function() {
+       cb(arguments);
+     });
    };
 
 }
